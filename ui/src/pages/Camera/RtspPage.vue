b792fda33631ab514b89ca55c90f50de979113dc
morax <ld.seraph@163.com>
2022-10-10 11:25:27 +0800

    new file:   ui/public/gb28181_camera.svg 	new file:   ui/public/rtsp_camera.svg 	new file:   ui/src/components/RexBreadcrumb.vue 	modified:   ui/src/i18n/zh-CN/index.ts 	new file:   ui/src/pages/Camera/RtspPage.vue 	modified:   ui/src/router/routes.ts 	modified:   ui/src/stores/sidebar-store.ts

---
+++ ui/src/pages/Camera/RtspPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page class="flex flex-col" padding>
+    <div class="basis-full flex items-center">
+      <RexBreadcrumb/>
+    </div>
+    <div class="basis-full pt-6">
+      <q-tabs align="left" v-model="tab_index" class="text-primary">
+        <q-tab :label="t('pages.camera.tables')" name="tables" />
+        <q-tab :label="t('pages.camera.groups')" name="groups" />
+        <q-tab :label="t('pages.camera.map')" name="map" />
+      </q-tabs>
+
+      <q-tab-panels v-model="tab_index" animated>
+        <q-tab-panel name="tables">
+          <CameraTable/>
+        </q-tab-panel>
+
+        <q-tab-panel name="groups">
+          <CameraGroup/>
+        </q-tab-panel>
+      </q-tab-panels>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import RexBreadcrumb from '@/components/RexBreadcrumb.vue';
+
+import CameraTable from './CameraTable.vue';
+import CameraGroup from './CameraGroup.vue'
+const { t } = useI18n();
+const tab_index = ref('tables');
+</script>
